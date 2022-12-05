a6e5bf0c801f16f72de753f9b33853d459122ec1
Jini <116340323+jini9256@users.noreply.github.com>
2022-12-05 21:26:23 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,15 +1,5 @@
-
-3
-4
-5
-6
-7
-8
-9
-10
-11
 function solution(n, numlist) {
-    var answer = [];
+    let answer = [];
 
     for (let i =0; i<= numlist.length; i++) {
       if(numlist[i] % n === 0) {
